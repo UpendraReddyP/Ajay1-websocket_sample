2ef2b48edf5f838f0870446ed9dd7e374ec6ceba
AjayKumar0121 <ajaykumar.v@astrolitetech.com>
2025-07-22 12:11:19 +0530

    Delete receiverServer.js

--- receiverServer.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'receiver')));
-app.listen(5502, () => console.log('📦 Receiver running on http://localhost:5502'));
